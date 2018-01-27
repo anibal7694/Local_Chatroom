95cac3e722ac03b8d64b76982e5ebe3a17e5b877
anibal7694 <anirudh.balaji94@gmail.com>
2018-01-26 19:49:40 -0500

    first commit

---
+++ AndroidChatClientServer/src/androidchatclientserver/SimpleChatServer.java
@@ -0,0 +1,32 @@
+package androidchatclientserver;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * This is the main class for chat server.
+ */
+public class SimpleChatServer {
+
+    public static void main(String[] args) {
+
+        ServerSocket serverSocket = null;
+        Socket clientSocket = null;
+
+        try {
+            serverSocket = new ServerSocket(4444);
+            System.out.println("Server started. Listening to the port 4444. Waitng for the client.");
+            clientSocket = serverSocket.accept();
+            System.out.println("Client connected on port 4444.");
+        } catch (IOException e) {
+            System.out.println("Could not listen on port: 4444");
+            e.printStackTrace();
+            return;
+        }
+
+        ChatWindow chatWindow = new ChatWindow();
+        chatWindow.open(clientSocket);
+    }
+
+}
